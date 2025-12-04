4f306752941b85d64b83bb85bf2607a1c8641913
Marco-Chen-06 <marcochen.work@gmail.com>
2025-12-04 01:49:05 -0500

    made some changes to matthew code in sem.c and sem.h

--- src/sem.c
+++ src/sem.c
@@ -1,49 +1,206 @@
 #include "sem.h"
 
+int my_procnum = 0;
+
+void blank_handler(int sn) {
+    //does nothing, just prevents process from terminating
+}
+
 void sem_init(struct sem *s, int count) {
-    s->lock = 0;
-    s->supply = count;
-    for (int i = 0; i < N_PROC; i++) {
-        s->sleepers[i] = 0;
-    }
-}
+    s->semaphore = count;
+    s->semaphore_lock = 0; //start off as unlocked obviously
 
-int sem_try(struct sem *s) {
-    spin_lock(s->lock);
-    // if "P" operation would block, return 0
-    if (s->supply <= 0) {
-        spin_unlock(s->lock);
-        return 0;
-    }
-    spin_unlock(s->lock);
-    return 1;
-}
-
-// unfinished
-void sem_wait(struct sem *s) {
-    spin_lock(s->lock);
-}
-
-void sem_inc(struct sem *s) {
-    spin_lock(s->lock);
-
-    // wake everyone up because supply went from 0 to 1
-    if (s->supply == 0) {
-        s->supply++;
-        for (int i = 0; i < s->num_sleepers; i++) {
-            // not sure if im allowed to put an integer into kill
-            kill(s->sleepers[i], SIGUSR1);
-        }
-        s->num_sleepers = 0;
-        spin_unlock(s->lock);
+    // early return if count is larger than our max process number
+    if(!(count <= N_PROC)) {
+        fprintf(stdout, "count is larger than maximum resource pool");
         return;
     }
 
-    // in every other case just increment supply like normal
-    s->supply++;
-    spin_unlock(s->lock);
-    return;
+    for(int i = 0; i < N_PROC; i++) {
+        s->wait_list[i] = 0;
+        s->sleep_list[i] = 0;
+    }
+
+    // s->wait_list[my_procnum] = getpid();
+
+
+    struct sigaction SIGUSR1_response;
+    SIGUSR1_response.sa_flags = 0;
+    sigemptyset(&SIGUSR1_response.sa_mask);
+    SIGUSR1_response.sa_handler = blank_handler;
+
+    if(sigaction(SIGUSR1, &SIGUSR1_response, NULL) == -1) {
+        perror("sigaction for assigning new handler for SIGUSR1 failed");
+        return;
+    }
+
+    //index is the next element (process) TO BE added, meaning currently no waiting process id is stored in wait_list[wait_list_index]
 }
+
+
+int sem_try(struct sem *s) {  //attempt P operation
+
+    sigset_t oldset; //will contain previous signal mask
+    sigemptyset(&oldset); 
+
+    sigset_t newset;
+    sigemptyset(&newset);
+
+    sigfillset(&newset); //blocks all signals
+
+    if(sigprocmask(SIG_SETMASK, &newset, &oldset) == -1) {
+        perror("signal masking failed");
+    }
+
+    spin_lock(&s->semaphore_lock);
+
+    if(s->semaphore == 0) {
+        spin_unlock(&s->semaphore_lock);
+        sigprocmask(SIG_SETMASK, &oldset, NULL); //reset signal mask to original
+        return 0;
+    }
+    
+    s->semaphore--;
+
+    spin_unlock(&s->semaphore_lock);
+
+    if(sigprocmask(SIG_SETMASK, &oldset, NULL) == -1) { //reset original mask
+        perror("signal masking failed");
+        return -1;
+    }
+
+    return 1;
+}
+
+void sem_wait(struct sem *s) { // P operation
+
+    sigset_t oldset; //will contain previous signal mask
+    sigemptyset(&oldset); 
+
+    sigset_t newset;
+    sigemptyset(&newset);
+
+    sigfillset(&newset); //blocks all signals
+
+    if(sigprocmask(SIG_SETMASK, &newset, &oldset) == -1) {
+        perror("signal masking failed");
+    }
+
+    spin_lock(&s->semaphore_lock);
+
+    while(s->semaphore == 0) {
+        s->wait_list[my_procnum] = getpid();
+        s->sleep_list[my_procnum] = 1; //add the current process to list of processes in wait list
+        spin_unlock(&s->semaphore_lock);
+        // sigprocmask(SIG_SETMASK, &oldset, NULL); //reset signal mask to original
+        sigemptyset(&newset);
+        sigsuspend(&newset); //any signal works, goes to sleep (blocks)
+        spin_lock(&s->semaphore_lock);
+    }
+    
+    s->semaphore--;
+
+    spin_unlock(&s->semaphore_lock);
+
+    
+    if(sigprocmask(SIG_SETMASK, &oldset, NULL) == -1) { //reset original mask
+        perror("signal masking failed");
+    }
+
+}
+
+void sem_inc(struct sem *s) { // V operation
+
+    sigset_t oldset; //will contain previous signal mask
+    sigemptyset(&oldset);
+
+    sigset_t newset; 
+    sigemptyset(&newset);
+
+    sigfillset(&newset); //blocks all signals
+
+    if(sigprocmask(SIG_SETMASK, &newset, &oldset) == -1) { //mask all signals
+        perror("signal masking failed");
+    }
+    
+    spin_lock(&s->semaphore_lock);
+
+    s->semaphore++;
+
+    for(int i = 0; i < N_PROC; i++) {
+        kill(s->wait_list[i], SIGUSR1); //wakes up every sleeping process
+    } 
+
+    for(int i = 0; i < N_PROC; i++) {
+        s->wait_list[i] = 0; //empties the waitlist
+    }
+
+    spin_unlock(&s->semaphore_lock);
+
+    if(sigprocmask(SIG_SETMASK, &oldset, NULL) == -1) { //reset original mask
+        perror("signal masking failed");
+    }
+}
+
+
+
+// marco old code for reference
+
+
+
+// #include "sem.h"
+
+// void sem_init(struct sem *s, int count) {
+//     s->lock = 0;
+//     s->supply = count;
+//     for (int i = 0; i < N_PROC; i++) {
+//         s->sleepers[i] = 0;
+//     }
+// }
+
+// int sem_try(struct sem *s) {
+//     spin_lock(s->lock);
+//     // if "P" operation would block, return 0
+//     if (s->supply <= 0) {
+//         spin_unlock(s->lock);
+//         return 0;
+//     }
+//     s->supply--;
+//     spin_unlock(s->lock);
+//     return 1;
+// }
+
+// // unfinished
+// void sem_wait(struct sem *s) {
+//     spin_lock(s->lock);
+
+//     // is this all that is necessary for suspending? (not sure)
+//     sigset_t empty_mask;
+//     sigemptyset(&empty_mask);
+//     sigsuspend(&empty_mask);
+// }
+
+
+// void sem_inc(struct sem *s) {
+//     spin_lock(s->lock);
+
+//     // wake everyone up because supply went from 0 to 1
+//     if (s->supply == 0) {
+//         s->supply++;
+//         for (int i = 0; i < s->num_sleepers; i++) {
+//             // not sure if im allowed to put an integer into kill
+//             kill(s->sleepers[i], SIGUSR1);
+//         }
+//         s->num_sleepers = 0;
+//         spin_unlock(s->lock);
+//         return;
+//     }
+
+//     // in every other case just increment supply like normal
+//     s->supply++;
+//     spin_unlock(s->lock);
+//     return;
+// }
 
 
 
--- src/sem.h
+++ src/sem.h
@@ -1,15 +1,48 @@
-#include <stdio.h>
-#include <stdlib.h>
+// changes: renamed n_proc to N_PROC
+// moved struct to header file
+// removed the -1 return value from sem_init on error because we don't anticipate errors from lecture notes
+// changed if(!(count <= my_procnum)) to if(!(count <= N_PROC)) to make it easier to compile and my_procnum is uninitialized
+// used sigemptyset instead of setting SIGUSR1_response to 0 in sem_init because SIGUSR1_response is not an int
+// moved the return 1 all the way to the bottom of sem_try
+// added "s->wait_list[my_procnum] = getpid();" to while loop in sem_wait
+// commented  s->wait_list[my_procnum] = getpid(); in sem_init
+// commented  sigprocmask(SIG_SETMASK, &oldset, NULL); //reset signal mask to original in sem_wait
+
 #include "spinlock.h"
+#define N_PROC 64
 
 struct sem {
-    int supply;
-    volatile char *lock;
-    int num_sleepers; 
-    int sleepers[N_PROC]; // stores PID of the sleeper in each index. If there is no sleeper, then element should be 0.
+    volatile unsigned int semaphore; //protects the data_example
+    volatile char semaphore_lock; //protects the semaphore integer 
+    pid_t wait_list[N_PROC]; //keeps track of processes waiting 
+    int sleep_list[N_PROC]; //flag array (0 = awake, 1 = asleep)
 };
+
+extern int my_procnum;
 
 void sem_init(struct sem *s, int count);
 int sem_try(struct sem *s);
 void sem_wait(struct sem *s);
 void sem_inc(struct sem *s);
+
+
+
+
+
+
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include "spinlock.h"
+
+// struct sem {
+//     int supply;
+//     volatile char *lock;
+//     int num_sleepers; 
+//     int sleepers[N_PROC]; // stores PID of the sleeper in each index. If there is no sleeper, then element should be 0.
+// };
+
+// void sem_init(struct sem *s, int count);
+// int sem_try(struct sem *s);
+// void sem_wait(struct sem *s);
+// void sem_inc(struct sem *s);
